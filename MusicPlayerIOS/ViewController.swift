271f4e270d6fae41e55ca36f87391ee0b7d1b05e
Ryan Weiler <ryan-wlr@ryans-mac.local>
2017-03-30 15:17:59 -0700

    Initial Commit

---
+++ MusicPlayerIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicPlayerIOS
+//
+//  Created by Ryan Weiler on 3/30/17.
+//  Copyright © 2017 Ryan Weiler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
